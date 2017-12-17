7529a55ea42ac021698b0ff0599e298a9d88a913
Ening <eningapps@gmail.com>
2017-12-17 14:23:05 +0300

    2c

--- src/main/java/com/quizapp/quiz/note/NoteController.java
+++ src/main/java/com/quizapp/quiz/note/NoteController.java
@@ -13,7 +13,6 @@ import java.util.List;
 @RequestMapping("/api")
 public class NoteController {
 
-    private static final String[] ANSWERS={"teplovoz","bkg1","chme3","dr1p","drb1","epg","epr","er9t","ten60","vl80"};
     
     @Autowired
     NoteRepository noteRepository;
@@ -22,15 +21,7 @@ public class NoteController {
     public List<Note> getAllNotes() {
         return noteRepository.findAll();
     }
-
-    @GetMapping("/answers")
-    public String getAnswers(){
-        String rez="";
-        for(String s:ANSWERS)
-            rez+=s+" ";
-        rez=rez.substring(0,rez.length()-1);
-        return rez;
-    }
+    
 
     @PostMapping("/notes")
     public Note createNote(@Valid @RequestBody Note note) {
